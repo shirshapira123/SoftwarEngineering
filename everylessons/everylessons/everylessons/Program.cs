2b2f7ea25805d0609f0651fb1fa65bedd9adabd5
shir <b33860@ganyavne.ort.org.il>
2020-01-13 09:39:18 +0200

    bull pgia

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -4,9 +4,55 @@ namespace everylessons
 {
     class Program
     {
+        public static int CheckBull(int[] randoms, int[] guesses)
+        {
+            int bulls = 0;
+            for (int i=0; i<4; i++)
+            {
+                if (randoms[i] == guesses[i])
+                {
+                    bulls++;
+                }
+            }
+            return bulls;
+        }
+        public static int CheckHit(int[] randoms, int[] guesses)
+        {
+            int hits = 0;
+            for (int i=0; i<4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (randoms[i] == guesses[j] && randoms[i] != guesses[i]) 
+                    {
+                        hits++;
+                    }
+                }
+            }
+            return hits;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("gittt mother fucker");
+            Random rnd = new Random();
+            int random = rnd.Next(1000, 10000);
+            int[] randoms = new int[4];
+            int[] guesses = new int[4];
+            int guess = int.Parse(Console.ReadLine());
+            for (int i=0; i<4; i++)
+            {
+                randoms[i] = random % 10;
+                random /= 10;
+            }
+            for (int i=0; i<4; i++)
+            {
+                guesses[i] = guess % 10;
+                guess /= 10;
+            }
+            int hits = CheckHit(randoms, guesses);
+            int bull = CheckBull(randoms, guesses);
+            
+            Console.ReadKey();
         }
     }
 }
