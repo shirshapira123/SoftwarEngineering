b2138a99c9b17d088924f3090e49e35cdb421679
shir <b33860@ganyavne.ort.org.il>
2020-01-13 10:55:51 +0200

    bull pgia

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -35,23 +35,30 @@ namespace everylessons
         static void Main(string[] args)
         {
             Random rnd = new Random();
+            int bulls=0;
             int random = rnd.Next(1000, 10000);
+            int random_to_print = random;
             int[] randoms = new int[4];
-            int[] guesses = new int[4];
-            int guess = int.Parse(Console.ReadLine());
-            for (int i=0; i<4; i++)
+            for (int i = 0; i < 4; i++)
             {
                 randoms[i] = random % 10;
                 random /= 10;
             }
-            for (int i=0; i<4; i++)
+            while (bulls<4)
             {
-                guesses[i] = guess % 10;
-                guess /= 10;
+                int[] guesses = new int[4];
+                int guess = int.Parse(Console.ReadLine());   
+                for (int i = 0; i < 4; i++)
+                {
+                    guesses[i] = guess % 10;
+                    guess /= 10;
+                }
+                int hits = CheckHit(randoms, guesses);
+                bulls = CheckBull(randoms, guesses);
+                Console.WriteLine("bulls:      hits:");
+                Console.WriteLine(" {0}         {1} ",bulls,hits);
             }
-            int hits = CheckHit(randoms, guesses);
-            int bull = CheckBull(randoms, guesses);
-            
+            Console.WriteLine("You Won!!");
             Console.ReadKey();
         }
     }
