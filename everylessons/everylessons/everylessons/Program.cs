8864234493c85284a1192d9df2bfd8e4820fa60c
shir <b33860@ganyavne.ort.org.il>
2020-01-22 10:34:19 +0200

    21/01/2020

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -4,11 +4,27 @@ namespace everylessons
 {
     class Program
     {
-      
+
         static void Main(string[] args)
         {
-      Console.ReadKey();
+            int[] flowers = new int[10];
+            for (int i = 0; i < flowers.Length; i++)
+                flowers[i] = int.Parse(Console.ReadLine());
+            int[] flowers2 = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                if (flowers[i] / 100 == 2)
+                {
+                    flowers2[i] = flowers[i];
+                    flowers[i] = 0;
+                }
+            }
+            Console.WriteLine(flowers);
+
+            Console.ReadKey();
         }
     }
 }
-//זכויות יוצרים למגניבים המדליקים
+
+                //Array.Copy(flowers,);
+                //זכויות יוצרים למגניבים המדליקים
