14fe5909bfe424eb1c36aa8dc334457de8542e54
shir <b33860@ganyavne.ort.org.il>
2020-02-11 11:41:09 +0200

    Binary search 11/2/2020

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -4,50 +4,45 @@ namespace everylessons
 {
     class Program
     {
+        public static bool BinarySearch(int[] arr, int num)
+        {
+            int half = arr.Length;
+            int low1 = 0, high1 = half / 2 - 1, low2 = high1, high2=arr.Length-1, middle1 = -1, middle2 = -1;
+            while (low1 <= high1)
+            {
+                middle1 = (low1 + high1) / 2;
+                if (num == arr[middle1])
+                    low1 = high1 + 1;
+                else if (num < arr[middle1])
+                    high1 = middle1 - 1;
+                else
+                    low1 = middle1 + 1;
+            }
+            while (low2 <= high2)
+            {
+                middle2 = (low2 + high2) / 2;
+                if (num == arr[middle2])
+                    low2 = high2 + 1;
+                else if (num < arr[middle2])
+                    high2 = middle2 - 1;
+                else
+                    low2 = middle2 + 1;
+            }
+            if (middle1 != 14 && middle2 != 14)
+                return true;
+            return false;
+            
+            
+        }
         static void Main(string[] args)
         {
-            Console.Write("how many chars? ");
-            int vsLength = int.Parse(Console.ReadLine());
-            char[] vs = new char[vsLength];
-            for (int i = 0; i < vs.Length; i++)
+            while (true)
             {
-                char charim = char.Parse(Console.ReadLine());
-                vs[i] = charim;
+                int[] vs = new int[15] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 5, 4, 3, 2, 1 };
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(BinarySearch(vs, num));
             }
-            int[] amount = new int[26];
-            for (int i = 0; i < vsLength; i++)
-            {
-                if ((int)vs[i] >= 97 && (int)vs[i] <= 123)
-                    amount[(int)vs[i] - 97]++;
-                else if ((int)vs[i] >= 65 && (int)vs[i] <= 91)
-                    amount[(int)vs[i] - 65]++;
-            }
-            int lastCounter = amount[0];
-            int check = 0;
-            for (int i = 0; i < amount.Length; i++)
-            {
-                if (amount[i] == lastCounter)
-                {
-                    check++;
-                }
-            }
-            if (check == vsLength)
-                Console.WriteLine("there is izogram.");
-            else
-                Console.WriteLine("there is no izogram.");
-            check = 0;
-            for (int i = 0; i < amount.Length; i++)
-            {
-                if (amount[i] == 0)
-                {
-                    check++;
-                }
-            }
-            if (check == 0)
-                Console.WriteLine("There is penegram");
-            else
-                Console.WriteLine("There is no penegram");
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
