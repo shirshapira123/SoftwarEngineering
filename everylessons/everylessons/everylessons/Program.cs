a06c0c00b5b71c837c01c3af745a1b69e191b341
shir <b33860@ganyavne.ort.org.il>
2020-02-19 11:28:55 +0200

    Sort - very importantgit add *!

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -4,8 +4,29 @@ namespace everylessons
 {
     class Program
     {
+        public static void Sort(int[] grades)
+        {
+            int t;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                for (int k = i+1; k < grades.Length; k++)
+                {
+                    if (grades[i] > grades[k])
+                    {
+                        t = grades[i];
+                        grades[i] = grades[k];
+                        grades[k] = t;
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            int[] grades = new int[200];
+            Random rnd = new Random();
+            for (int i = 0; i < grades.Length; i++)
+                grades[i] = rnd.Next(0,100);
+            Sort(grades);
 
         }
     }
