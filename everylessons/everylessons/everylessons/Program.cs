5bd7429b6f75ce9e1f59a021e73ca75dfd62826a
shir <b33860@ganyavne.ort.org.il>
2020-05-03 15:44:12 +0300

    page 99 exercise 40

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -4,28 +4,18 @@ namespace everylessons
 {
     class Program
     {
+        public static bool Takin (Domino [] dominos)
+        {
+            
+            for (int i = 1; i < dominos.Length; i++)
+            {
+                if (dominos[i].GetRight() != dominos[i - 1].GetLeft())
+                    return false;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
-            int[] shows = new int[16];
-            int show = 1, tikets, max = 0;
-            while (show != 0)
-            {
-                Console.WriteLine("enter show number");
-                show = int.Parse(Console.ReadLine());
-                Console.WriteLine("enter number of tickets");
-                tikets = int.Parse(Console.ReadLine());
-                shows[show] += tikets;
-            }
-            for (int i = 1; i < shows.Length; i++)
-                Console.WriteLine("show: {0}, tikets: {1}", i, shows[i]);
-            for (int i = 0; i < shows.Length; i++)
-            {
-                if (shows[i] > max)
-                    max = i;
-            }
-            Console.WriteLine(max);
-
-
 
         }
     }
