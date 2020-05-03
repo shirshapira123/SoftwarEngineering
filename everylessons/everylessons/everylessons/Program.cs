d06c0c19616616254e3b4a1327018ccc986f6f21
shir <b33860@ganyavne.ort.org.il>
2020-05-03 14:30:12 +0300

    page 86 exercise 27

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -6,23 +6,40 @@ namespace everylessons
     { 
         static void Main(string[] args)
         {
-            int[] counter = new int[11];
-            int i, num;
-            for (i = 0; i < counter.Length; i++)
+            const int N = 31;
+            int[] a = new int[N];
+            int i, j, workers, min = 1, max = 0, imax = 0, jmax = 0;
+            for (i = 0; i < a.Length; i++)
             {
-                counter[i] = 0; 
+                a[i] = 0;
             }
-            for (i = 0; i <= 1000; i++)
+            for (i = 1; i < N; i++)
             {
-                Console.WriteLine("enter a number");
-                num = int.Parse(Console.ReadLine());
-                if (num >= 1 && num <= 10)
-                    counter[num]++;
+                Console.WriteLine("floor " + i);
+                for (j = 1; j <= 7; j++)
+                {
+                    Console.WriteLine("enter the amount of people in office number " + j);
+                    workers = int.Parse(Console.ReadLine());
+                    a[i] += workers;
+                    if (workers > max)
+                    {
+                        max = workers;
+                        imax = i;
+                        jmax = j;
+                    }
+                }
             }
-            for (i = 1; i <= 10; i++)
+            for (i = 1; i < N; i++)
             {
-                Console.WriteLine(i + ", " + counter[i]);
+                Console.WriteLine("floor " + i + "," +a[i]);
             }
+            for (i = 1; i < N; i++)
+            {
+                if (a[i] < a[min])
+                    min = i;
+            }
+            Console.WriteLine("the floor that have the smallest amount of workers is " + min + "the number of workers in this floor is " + a[min]);
+            Console.WriteLine ("the office that has the biggest amount of workers is office " + jmax + " on floor " + imax + " the number of workers in this office is " + max);                      
         }
     }
 }
