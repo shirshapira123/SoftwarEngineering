1b32220f3510a1666e47086ab2b21437a1d386e7
shir <b33860@ganyavne.ort.org.il>
2020-05-04 17:27:18 +0300

    penegram & izogram

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -4,84 +4,110 @@ namespace everylessons
 {
     class Program
     {
-        static void ExerciseOne(int [,] cinema)
+        public static void isometric(char[] arr, int length)
         {
-            for (int i = 0; i < cinema.GetLength(0); i++)
-            {
-                for (int k = 0; k < cinema.GetLength(1); k++)
-                {
-                    cinema[i, k] = 0;
-                }
-            }
-        }
-        static bool ExerciseTwo(int chair, int row, int[,] cinema)
-        {
-            if (cinema[row, chair] == 0)
-            {
-                return true;
-            }
-            return false;
-        }
-        static void ExerciseThree(int chair, int row, int[,] cinema)
-        {
-            if (ExerciseTwo(chair,row, cinema))
-            {
-                cinema[row, chair] = 1;
-                Console.WriteLine("verified shopping");
-            }
-            else
-                Console.WriteLine("this place isn't available.");
-        }
-        static void ExerciseFour(int[,] cinema)
-        {
-            int available = 0;
-            int all_available = 0;
-            for (int i = 0; i < cinema.GetLength(0); i++)
-            {
-                for (int k = 0; k < cinema.GetLength(1); k++)
-                {
-                    if (cinema[i, k] == 0)
-                    {
-                        available++;
-                        all_available++;
-                    }
-                }
-                Console.WriteLine("there are " +available+ " available places in line " +(i+1));
-                available = 0;
-            }
-            Console.WriteLine("there are " + all_available + " available places in all lines");
-        }
-        static void ExerciseFive(int[,] cinema)
-        {
-            int not_available = 0;
-            for (int i = 0; i < cinema.GetLength(0); i++)
-            {
-                for (int k = 0; k < cinema.GetLength(1); k++)
-                {
-                    if(cinema[i, k] == 1)
-                    not_available++;
-                }
-            }
-            Console.WriteLine("there are " + not_available + " places which bought in all lines");
-        }
+            int[] arrey = new int[26];
+            int last = 0;
 
+            for (int i = 0; i < length; i++)
+            {
+                int chr = (int)arr[i];
+
+                if (chr >= 97 && chr <= 123)
+                {
+                    arrey[chr - 97]++;
+                    last = arrey[chr - 97];
+                }
+                else if (chr >= 65 && chr <= 91)
+                {
+                    arrey[chr - 65]++;
+                    last = arrey[chr - 65];
+                }
+            }
+
+            bool isOK = true;
+
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                if (arrey[i] > 0 && arrey[i] != last)
+                {
+                    isOK = false;
+                    break;
+                }
+            }
+
+            if (isOK)
+                Console.WriteLine("Isogram");
+            else
+                Console.WriteLine("Not Isogram");
+        }
+        public static char[] Kelet()
+        {
+            Console.WriteLine("Input the word's length");
+            int length = int.Parse(Console.ReadLine());
+            char[] arr = new char[length];
+            Console.WriteLine("Enter word letter by letter");
+            for (int b = 0; b < length; b++)
+            {
+                char a = Char.Parse(Console.ReadLine());
+                arr[b] = a;
+            }
+            return arr;
+        }
+        public static void ispanagram(char[] arr, int length)          {
+            int[] arrey = new int[26];
+            int last = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                int chr = (int)arr[i];
+
+                if (chr >= 97 && chr <= 123)
+                {
+                    arrey[chr - 97]++;
+                    last = arrey[chr - 97];
+                }
+                else if (chr >= 65 && chr <= 91)
+                {
+                    arrey[chr - 65]++;
+                    last = arrey[chr - 65];
+                }
+            }
+
+            bool isOK = true;
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                if (arrey[i] == 0 || arrey[i] != last)
+                {
+                    isOK = false;
+                    break;
+                }
+            }
+            bool isPan = true;
+
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                if (arrey[i] == 0)
+                {
+                    isPan = false;
+                    break;
+                }
+            }
+            if (isPan)
+                Console.WriteLine("A panagram");
+            if (isOK && last == 1)
+                Console.WriteLine("This is a perfect Panagram");
+            if (!isPan)
+                Console.WriteLine("Not a Panagram");
+        }
         static void Main(string[] args)
         {
-            int[,] cinema = new int[10,14];
-            ExerciseOne(cinema);
-            for (int i = 0; i < 100; i++)
-            {
-                Console.Write("row: ");
-                int row = int.Parse(Console.ReadLine());
-                Console.Write("chair: ");
-                int chair = int.Parse(Console.ReadLine());
-                ExerciseThree(chair-1, row-1, cinema);
-            }
-            ExerciseFour(cinema);
-            ExerciseFive(cinema);
+            char[] arr = Kelet();
+            isometric(arr, arr.Length);
+            ispanagram(arr, arr.Length);
             Console.ReadKey();
-
         }
     }
 }
-    //זכויות יוצרים למגניבים המדליקים   
+
+//זכויות יוצרים למגניבים המדליקים   
