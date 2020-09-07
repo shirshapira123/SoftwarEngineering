3d9a7f56b25d97d52e2ee70c671d0f4edbbbfb6c
shir <b33860@ganyavne.ort.org.il>
2020-09-07 12:28:57 +0300

    dates

---
+++ everylessons/everylessons/everylessons/Date.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everylessons
+{
+    class Date
+    {
+        int year, month, day;
+        public Date (int year, int month, int day)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+        
+        public int GetYear() => year;
+        public int GetMonth() => month;
+        public int GetDay() => day;
+        public void SetYear(int year)
+        {
+            this.year = year;
+        }
+        public void SetMonth(int month)
+        {
+            this.month = month;
+        }
+        public void SetDay(int day)
+        {
+            this.day = day;
+        }
+        public int CompareTo(Date other)
+        {
+            int checkYear = year - other.GetYear();
+            int checkMonth = month - other.GetMonth();
+            int checkDay = day - other.GetDay();
+            if (checkYear != 0)
+                return checkYear;
+            if (checkMonth != 0)
+                return checkMonth;
+            return checkDay;
+        }
+        public override string ToString() => (day + "." + month + "." + year);
+    }
+}
--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -6,23 +6,30 @@ namespace everylessons
     {
         static void Main(string[] args)
         {
-            Dice dice1 = new Dice();
-            Dice dice2 = new Dice();
-            DiceGame diceGame = new DiceGame(dice1, dice2);
-            bool flag = false;
-            while (!flag)
-            {
-                Console.ReadKey();
-                Console.Write("roll number " + diceGame.GetTimes() + ":");
-                string rolling = diceGame.Rolling();
-                Console.WriteLine(rolling);
-                flag = diceGame.WinOrNot();
-                if (!flag)
-                {
-                    Console.WriteLine("You lose this round, try again");
-                }
-            }
-            Console.WriteLine("YOU WIN THE GAME!!, you rolled the dice " + (diceGame.GetTimes()-1) + " times.");
+            Console.Write("enter first date:\nYear: ");
+            int year1 = int.Parse(Console.ReadLine());
+            Console.Write("month: ");
+            int month1 = int.Parse(Console.ReadLine());
+            Console.Write("day: ");
+            int day1 = int.Parse(Console.ReadLine());
+            Console.Write("enter second date:\nYear: ");
+            int year2 = int.Parse(Console.ReadLine());
+            Console.Write("month: ");
+            int month2 = int.Parse(Console.ReadLine());
+            Console.Write("day: ");
+            int day2 = int.Parse(Console.ReadLine());
+            Date date1 = new Date(year1, month1, day1);
+            Date date2 = new Date(year2, month2, day2);
+            Console.WriteLine("date N.1: " + date1.ToString());
+            Console.WriteLine("date N.2: " + date2.ToString());
+            int compare = date1.CompareTo(date2);
+            if(compare > 0)
+                Console.WriteLine("date number 1 is latest.");
+            else if(compare < 0)
+                Console.WriteLine("date number 2 is latest.");
+            else
+                Console.WriteLine("these dates are equale");
+            Console.WriteLine(date1);
             Console.ReadKey();
         }
     }
