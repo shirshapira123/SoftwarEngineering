430db36b1fdb223bae739d91e0771bd9b46a1e21
shir <b33860@ganyavne.ort.org.il>
2020-09-16 18:07:05 +0300

    arrays

---
+++ everylessons/everylessons/everylessons/MyArrayMethods.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everylessons
+{
+    public static class MyArrayMethods
+    {
+        public static void One(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = 0;
+        }
+        public static void Two(int[] arr)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+        }
+        public static void Three(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+        public static void Four(int[] arr)
+        {
+            for (int i = arr.Length-1; i >= 0; i--)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+        public static void Five(int[] arr)
+        {
+            Console.Write("search num: ");
+            int search = int.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (search == arr[i])
+                {
+                    Console.WriteLine("the num placed in: ");
+                    Console.WriteLine(i+1);
+                }
+            }
+        }
+        public static void Six(int[] arr)
+        {
+            int counter = 0;
+            Console.Write("search num: ");
+            int search = int.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (search == arr[i])
+                    counter++;
+            }
+            Console.WriteLine("the number is placed " + counter + " times");
+        }
+        public static void Seven(int[] arr)
+        {
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+                counter += arr[i];
+            Console.WriteLine(counter);
+        }
+        public static void Eight(int[] arr)
+        {
+            int swich = arr[0];
+            arr[0] = arr[arr.Length - 1];
+            arr[arr.Length - 1] = swich;
+        }
+        public static void Nine(int[] arr)
+        {
+            int lastNum = arr[arr.Length - 1];
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = lastNum;
+        }
+        public static void Ten(int[] arr)
+        {
+            int firstNum = arr[0];
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.Length - 1] = firstNum;
+        }
+        public static void Eleven(int[] arr)
+        {
+            bool sort = true;
+            for (int i = 0; i < arr.Length - 1; i++) 
+            {
+                if(arr[i] > arr[i + 1])
+                {
+                    sort = false;
+                }
+            }
+            Console.WriteLine(sort);
+        }
+        public static void Twelve(int[] arr)
+        {
+            int t;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[i] > arr[k])
+                    {
+                        t = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = t;
+                    }
+                }
+            }
+        }
+        public static int[] Thirteen(int[] arr1, int[] arr2)
+        {
+            int[] new_arr = new int[arr1.Length + arr2.Length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                new_arr[i] = arr1[i];
+            }
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                new_arr[i + arr1.Length] = arr2[i];
+            }
+            MyArrayMethods.Twelve(new_arr);
+            return new_arr;
+        }
+    }
+}
--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -6,30 +6,28 @@ namespace everylessons
     {
         static void Main(string[] args)
         {
-            Console.Write("enter first date:\nYear: ");
-            int year1 = int.Parse(Console.ReadLine());
-            Console.Write("month: ");
-            int month1 = int.Parse(Console.ReadLine());
-            Console.Write("day: ");
-            int day1 = int.Parse(Console.ReadLine());
-            Console.Write("enter second date:\nYear: ");
-            int year2 = int.Parse(Console.ReadLine());
-            Console.Write("month: ");
-            int month2 = int.Parse(Console.ReadLine());
-            Console.Write("day: ");
-            int day2 = int.Parse(Console.ReadLine());
-            Date date1 = new Date(year1, month1, day1);
-            Date date2 = new Date(year2, month2, day2);
-            Console.WriteLine("date N.1: " + date1.ToString());
-            Console.WriteLine("date N.2: " + date2.ToString());
-            int compare = date1.CompareTo(date2);
-            if(compare > 0)
-                Console.WriteLine("date number 1 is latest.");
-            else if(compare < 0)
-                Console.WriteLine("date number 2 is latest.");
-            else
-                Console.WriteLine("these dates are equale");
-            Console.WriteLine(date1);
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            MyArrayMethods.Three(arr);
+            MyArrayMethods.One(arr);
+            MyArrayMethods.Three(arr);
+            MyArrayMethods.Two(arr);
+            MyArrayMethods.Four(arr);
+            MyArrayMethods.Nine(arr);
+            MyArrayMethods.Three(arr);
+            MyArrayMethods.Ten(arr);
+            MyArrayMethods.Three(arr);
+            MyArrayMethods.Eleven(arr);
+            MyArrayMethods.Five(arr);
+            MyArrayMethods.Six(arr);
+            MyArrayMethods.Seven(arr);
+            MyArrayMethods.Eight(arr);
+            MyArrayMethods.Twelve(arr);
+            MyArrayMethods.Eleven(arr);
+            MyArrayMethods.Three(arr);
+            int[] arr2 = new int[5];
+            MyArrayMethods.Two(arr2);
+            int[] new_arr = MyArrayMethods.Thirteen(arr, arr2);
+            MyArrayMethods.Three(new_arr);
             Console.ReadKey();
         }
     }
