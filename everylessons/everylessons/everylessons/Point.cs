adc1c8d6778183526484fcc7d756fa45bdee0627
shir <b33860@ganyavne.ort.org.il>
2020-09-26 11:10:07 +0300

    class point Upgraded

---
+++ everylessons/everylessons/everylessons/Point.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everylessons
+{
+    class Point
+    {
+        double x, y;
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        public double GetX() => x;
+        public double GetY() => y;
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+        public double Distance(Point p)
+        {
+            double delthaX = Math.Pow(p.GetX() - x, 2);
+            double delthaY = Math.Pow(p.GetY() - y, 2);
+            return Math.Sqrt(delthaX + delthaY);
+        }
+        public Point Middle(Point p)
+        {
+            double xMiddle = (p.GetX() + x) / 2;
+            double yMiddle = (p.GetY() + y) / 2;
+            return new Point(xMiddle, yMiddle);
+        }
+        public int Quadrant()
+        {
+            if (x == 0 || y == 0)
+                return 0;
+            if (x > 0 && y > 0)
+                return 1;
+            if (x < 0 && y > 0)
+                return 2;
+            if (x < 0 && y < 0)
+                return 3;    
+            return 4;
+        }
+        public override string ToString() => ($"({x},{y})");
+        
+    }
+}
--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -6,29 +6,49 @@ namespace everylessons
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            MyArrayMethods.Three(arr);
-            MyArrayMethods.One(arr);
-            MyArrayMethods.Three(arr);
-            MyArrayMethods.Two(arr);
-            MyArrayMethods.Four(arr);
-            MyArrayMethods.Nine(arr);
-            MyArrayMethods.Three(arr);
-            MyArrayMethods.Ten(arr);
-            MyArrayMethods.Three(arr);
-            MyArrayMethods.Eleven(arr);
-            MyArrayMethods.Five(arr);
-            MyArrayMethods.Six(arr);
-            MyArrayMethods.Seven(arr);
-            MyArrayMethods.Eight(arr);
-            MyArrayMethods.Twelve(arr);
-            MyArrayMethods.Eleven(arr);
-            MyArrayMethods.Three(arr);
-            int[] arr2 = new int[5];
-            MyArrayMethods.Two(arr2);
-            int[] new_arr = MyArrayMethods.Thirteen(arr, arr2);
-            MyArrayMethods.Three(new_arr);
+            Point[] points = new Point[5];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine("Enter X: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Y: ");
+                int y = int.Parse(Console.ReadLine());
+                points[i] = new Point(x, y);
+            }
+            Point a = new Point();
+            for (int i = 0; i < points.Length; i++)
+                Console.WriteLine(points[i].ToString());
+            for (int i = 0; i < points.Length; i++)
+                Console.WriteLine($"Distace from point a to point number {i + 1}: " + points[i].Distance(a));
+            int quadrant_one = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (points[i].Quadrant() == 1)
+                    quadrant_one++;
+            }
+            Console.WriteLine(quadrant_one + " points in quadrant number 1");
+            points[1].SetX(points[1].GetX() - 3);
+            Console.WriteLine("point 2 after sliding: " + points[1].ToString());
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].SetY(points[i].GetY() - 5);
+                Console.WriteLine($"point {i+1} after sliding: {points[i].ToString()}");
+            }
+            Point middle = points[0].Middle(points[4]);
+            Console.WriteLine("middle betwin the first and last point: " + middle.ToString());
+
+            double max = 0;
+            int point_max = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                max = Math.Max(max, points[i].Distance(a));
+                if (points[i].Distance(a) == max)
+                    point_max = i;
+            }
+            Console.WriteLine(points[point_max].ToString());
+
             Console.ReadKey();
+
         }
     }
 }
