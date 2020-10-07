cbcb50e327dd0d016def9bab473fd7677c9e1ec9
shir <b33860@ganyavne.ort.org.il>
2020-10-07 21:31:08 +0300

    Line and polygon, continue code for 'class Point'

---
+++ everylessons/everylessons/everylessons/Line.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everylessons
+{
+    class Line
+    {
+        double x1, y1, x2, y2;
+        public Line(double x1, double y1, double x2, double y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        /// <summary>
+        /// return the first point
+        /// </summary>
+        /// <returns>start_point(Point)</returns>
+        public Point GetStartPoint()
+        {
+            Point start_point = new Point(x1, y1);
+            return start_point;
+        }
+        /// <summary>
+        /// return the last point
+        /// </summary>
+        /// <returns>end_point(Point)</returns>
+        public Point GetEndPoint()
+        {
+            Point end_point = new Point(x2, y2);
+            return end_point;
+        }
+        /// <summary>
+        /// set the start of the line
+        /// </summary>
+        /// <param name="x1">the x of the first point</param>
+        /// <param name="y1">the y of the first point</param>
+        public void SetStartPoint(double x1, double y1)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+        }
+        /// <summary>
+        /// set the end of the line
+        /// </summary>
+        /// <param name="x2">the x of the end point</param>
+        /// <param name="y2">the y of the end point</param>
+        public void SetEndPoint(double x2, double y2)
+        {
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        /// <summary>
+        /// the line description
+        /// </summary>
+        /// <returns>returns the line description(string)</returns>
+        public override string ToString()
+        {
+            return ($"({x1},{y1})--->({x2},{y2})");
+        }
+        /// <summary>
+        /// check if the line is horizontal
+        /// </summary>
+        /// <returns>if horizontal returns true else retruns false</returns>
+        public bool IsHorizontalLine()
+        {
+            if (x1 == x2)
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// check if the line is vertical
+        /// </summary>
+        /// <returns>if vertical returns true else retruns false</returns>
+        public bool IsVerticalLine()
+        {
+            if (y1 == y2)
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// calculate the length of the line
+        /// </summary>
+        /// <returns>length(double)</returns>
+        public double LengthLine()
+        {
+            Point start_point = new Point(x1, y1);
+            Point end_point = new Point(x2, y2);
+            return start_point.Distance(end_point);
+        }
+        /// <summary>
+        /// find the middle point (the point that placed in the middle of the line)
+        /// </summary>
+        /// <returns>the middle point(Point)</returns>
+        public Point GetMiddleLine()
+        {
+            Point start_point = new Point(x1, y1);
+            Point end_point = new Point(x2, y2);
+            return start_point.Middle(end_point);
+        }
+        /// <summary>
+        /// make the line look like a function
+        /// </summary>
+        /// <returns>returns fanction like string: mx+b(string)</returns>
+        public string FunctionLine()
+        {
+            double m = (y1 - y2) / (x1 - x2);
+            double b = m * x1 * (-1) + y1;
+            if (b == 0)
+                return $"y={m}x";
+            if (b > 0)
+                return $"y={m}x+{b}";
+            return $"y={m}x-{b}";
+        }
+        /// <summary>
+        /// that action get point and check if the point is on the line
+        /// </summary>
+        /// <param name="a">point to check</param>
+        /// <returns>if the point is on the line return true, else return false</returns>
+        public bool IsPointOnTheLine(Point a)
+        {
+            double m = (y1 - y2) / (x1 - x2);
+            double b = m * x1 * (-1) + y1;
+            double y = m*a.GetX()-b;
+            if (y == a.GetY())
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// Gets another line and checks if the current line and the extra line form a right angle
+        /// </summary>
+        /// <param name="otherLine">another line</param>
+        /// <returns>if the current line and the extra line form a right angle returns true else return true</returns>
+        public bool Is90AngleLines(Line otherLine)
+        {
+            double m1 = (y1 - y2) / (x1 - x2);
+            double m2 = (otherLine.GetStartPoint().GetY() - otherLine.GetEndPoint().GetY()) / (otherLine.GetStartPoint().GetX() - otherLine.GetEndPoint().GetX());
+            if (m1 / m2 == -1)
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// Gets another line and checks if the current line and the other line are parallel
+        /// </summary>
+        /// <param name="otherLine">another line</param>
+        /// <returns>if the current line and the extra line are parallel returns true else return true</returns>
+        public bool IsParallelLines(Line otherLine)
+        {
+            double m1 = (y1 - y2) / (x1 - x2);
+            double m2 = (otherLine.GetStartPoint().GetY() - otherLine.GetEndPoint().GetY()) / (otherLine.GetStartPoint().GetX() - otherLine.GetEndPoint().GetX());
+            if (m1 == m2)
+                return true;
+            return false;
+        }
+
+    }
+}
--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -6,49 +6,103 @@ namespace everylessons
     {
         static void Main(string[] args)
         {
-            Point[] points = new Point[5];
-            for (int i = 0; i < points.Length; i++)
-            {
-                Console.WriteLine("Enter X: ");
-                int x = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter Y: ");
-                int y = int.Parse(Console.ReadLine());
-                points[i] = new Point(x, y);
-            }
-            Point a = new Point();
-            for (int i = 0; i < points.Length; i++)
-                Console.WriteLine(points[i].ToString());
-            for (int i = 0; i < points.Length; i++)
-                Console.WriteLine($"Distace from point a to point number {i + 1}: " + points[i].Distance(a));
-            int quadrant_one = 0;
-            for (int i = 0; i < points.Length; i++)
-            {
-                if (points[i].Quadrant() == 1)
-                    quadrant_one++;
-            }
-            Console.WriteLine(quadrant_one + " points in quadrant number 1");
-            points[1].SetX(points[1].GetX() - 3);
-            Console.WriteLine("point 2 after sliding: " + points[1].ToString());
-            for (int i = 0; i < points.Length; i++)
-            {
-                points[i].SetY(points[i].GetY() - 5);
-                Console.WriteLine($"point {i+1} after sliding: {points[i].ToString()}");
-            }
-            Point middle = points[0].Middle(points[4]);
-            Console.WriteLine("middle betwin the first and last point: " + middle.ToString());
+            Console.WriteLine("first line:");
+            Console.Write("enter x1: ");
+            double x1 = int.Parse(Console.ReadLine());
+            Console.Write("enter y1: ");
+            double y1 = int.Parse(Console.ReadLine());
+            Console.Write("enter x2: ");
+            double x2 = int.Parse(Console.ReadLine());
+            Console.Write("enter y2: ");
+            double y2 = int.Parse(Console.ReadLine());
+            Line line1 = new Line(x1, y1, x2, y2);
+            Console.WriteLine("second line:");
+            Console.Write("enter x1: ");
+            x1 = int.Parse(Console.ReadLine());
+            Console.Write("enter y1: ");
+            y1 = int.Parse(Console.ReadLine());
+            Console.Write("enter x2: ");
+            x2 = int.Parse(Console.ReadLine());
+            Console.Write("enter y2: ");
+            y2 = int.Parse(Console.ReadLine());
+            Line line2 = new Line(x1, y1, x2, y2);
+            Console.WriteLine("start point of the first line: " + line1.GetStartPoint());
+            Console.WriteLine("last point of the second line: " + line2.GetEndPoint());
+            line2.SetStartPoint(0, 0);
+            Console.WriteLine("line 1: " + line1);
+            Console.WriteLine("line 2: " + line2);
+            Console.WriteLine("line 1 function: " + line1.FunctionLine());
+            Console.WriteLine("line 2 function: " + line2.FunctionLine());
+            Console.WriteLine("line 1 length: " + line1.LengthLine());
+            Console.WriteLine("line 2 length: " + line2.LengthLine());
+            Console.WriteLine("line 1 middle point: " + line1.GetMiddleLine());
+            if (line1.Is90AngleLines(line2))
+                Console.WriteLine("these two lines form a right angle");
+            else
+                Console.WriteLine("these two lines don't form a right angle");
+            if (line1.IsParallelLines(line2))
+                Console.WriteLine("these two lines are parallel");
+            else
+                Console.WriteLine("these two lines aren't parallel");
+            if (line1.IsVerticalLine())
+                Console.WriteLine("line 1 is vertical");
+            else
+                Console.WriteLine("line 1 is not vertical");
+            if (line2.IsHorizontalLine())
+                Console.WriteLine("line 2 is horizontal");
+            else
+                Console.WriteLine("line 2 is not horizontal");
 
-            double max = 0;
-            int point_max = 0;
-            for (int i = 0; i < points.Length; i++)
-            {
-                max = Math.Max(max, points[i].Distance(a));
-                if (points[i].Distance(a) == max)
-                    point_max = i;
-            }
-            Console.WriteLine(points[point_max].ToString());
 
+            Line[] polygon = new Line[10];
+            Random rnd = new Random();
+            double first_line_x = rnd.Next(-10, 11);
+            double first_line_y = rnd.Next(-10, 11);
+            x1 = rnd.Next(-10, 11);
+            y1 = rnd.Next(-10, 11);
+            polygon[0] = new Line(first_line_x, first_line_y, x1,y1);
+            for (int i = 1; i < polygon.Length-1; i++)
+            {
+                x2 = rnd.Next(-10, 11);
+                y2 = rnd.Next(-10, 11);
+                polygon[i] = new Line(x1, y1, x2, y2);
+                x1 = x2;
+                y1 = y2;
+            }
+            polygon[9] = new Line(x1, y1, first_line_x, first_line_y);
+            foreach (Line item in polygon)
+            {
+                Console.WriteLine(item);
+            }
+            int counter = 0;
+            for (int i = 0; i < polygon.Length; i++)
+            { 
+                    if (!polygon[i].IsHorizontalLine() && !polygon[i].IsVerticalLine()) 
+                        counter++;  
+            }
+            Console.WriteLine($"there are {counter} lines who aren't vertical and not horizontal");
+            bool parallel = false;
+            for (int i = 0; i < polygon.Length; i++)
+            {
+                for (int k = 0; k < polygon.Length; k++)
+                {
+                    if (polygon[i].IsParallelLines(polygon[k]) && i != k)
+                    {
+                        parallel = true;
+                    }
+                }
+            }
+            if(parallel)
+                Console.WriteLine("there are some parallel lines");
+            else
+                Console.WriteLine("there are 0 parallel lines");
+            double p = 0;
+            for (int i = 0; i < polygon.Length; i++)
+                p += polygon[i].LengthLine();
+            Console.WriteLine($"the circumference of the polygon is {p}");
+            Line connector = new Line(polygon[0].GetStartPoint().GetX(), polygon[0].GetStartPoint().GetY(), polygon[4].GetStartPoint().GetX(), polygon[4].GetStartPoint().GetY());
+            Console.WriteLine("new line betwin first point of line number 1 and first point of line number 5: " + connector);
             Console.ReadKey();
-
         }
     }
 }
