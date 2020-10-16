11eab9a437b820e612bb58c2762b6370d672cc18
shir <b33860@ganyavne.ort.org.il>
2020-10-16 17:05:45 +0300

    class, student, and parent

--- everylessons/everylessons/everylessons/Program.cs
+++ everylessons/everylessons/everylessons/Program.cs
@@ -4,104 +4,97 @@ namespace everylessons
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void ShowParents(Class group)
         {
-            Console.WriteLine("first line:");
-            Console.Write("enter x1: ");
-            double x1 = int.Parse(Console.ReadLine());
-            Console.Write("enter y1: ");
-            double y1 = int.Parse(Console.ReadLine());
-            Console.Write("enter x2: ");
-            double x2 = int.Parse(Console.ReadLine());
-            Console.Write("enter y2: ");
-            double y2 = int.Parse(Console.ReadLine());
-            Line line1 = new Line(x1, y1, x2, y2);
-            Console.WriteLine("second line:");
-            Console.Write("enter x1: ");
-            x1 = int.Parse(Console.ReadLine());
-            Console.Write("enter y1: ");
-            y1 = int.Parse(Console.ReadLine());
-            Console.Write("enter x2: ");
-            x2 = int.Parse(Console.ReadLine());
-            Console.Write("enter y2: ");
-            y2 = int.Parse(Console.ReadLine());
-            Line line2 = new Line(x1, y1, x2, y2);
-            Console.WriteLine("start point of the first line: " + line1.GetStartPoint());
-            Console.WriteLine("last point of the second line: " + line2.GetEndPoint());
-            line2.SetStartPoint(0, 0);
-            Console.WriteLine("line 1: " + line1);
-            Console.WriteLine("line 2: " + line2);
-            Console.WriteLine("line 1 function: " + line1.FunctionLine());
-            Console.WriteLine("line 2 function: " + line2.FunctionLine());
-            Console.WriteLine("line 1 length: " + line1.LengthLine());
-            Console.WriteLine("line 2 length: " + line2.LengthLine());
-            Console.WriteLine("line 1 middle point: " + line1.GetMiddleLine());
-            if (line1.Is90AngleLines(line2))
-                Console.WriteLine("these two lines form a right angle");
-            else
-                Console.WriteLine("these two lines don't form a right angle");
-            if (line1.IsParallelLines(line2))
-                Console.WriteLine("these two lines are parallel");
-            else
-                Console.WriteLine("these two lines aren't parallel");
-            if (line1.IsVerticalLine())
-                Console.WriteLine("line 1 is vertical");
-            else
-                Console.WriteLine("line 1 is not vertical");
-            if (line2.IsHorizontalLine())
-                Console.WriteLine("line 2 is horizontal");
-            else
-                Console.WriteLine("line 2 is not horizontal");
-
-
-            Line[] polygon = new Line[10];
-            Random rnd = new Random();
-            double first_line_x = rnd.Next(-10, 11);
-            double first_line_y = rnd.Next(-10, 11);
-            x1 = rnd.Next(-10, 11);
-            y1 = rnd.Next(-10, 11);
-            polygon[0] = new Line(first_line_x, first_line_y, x1,y1);
-            for (int i = 1; i < polygon.Length-1; i++)
+            for (int i = 0; i < group.GetMaxAmountStudent(); i++)
             {
-                x2 = rnd.Next(-10, 11);
-                y2 = rnd.Next(-10, 11);
-                polygon[i] = new Line(x1, y1, x2, y2);
-                x1 = x2;
-                y1 = y2;
+                Console.WriteLine(group.GetStudents()[i].GetParent());
             }
-            polygon[9] = new Line(x1, y1, first_line_x, first_line_y);
-            foreach (Line item in polygon)
+        }
+        public static bool UpdateParentPhone(Class group, string name, string phone)
+        {
+            for (int i = 0; i < group.GetMaxAmountStudent(); i++)
             {
-                Console.WriteLine(item);
-            }
-            int counter = 0;
-            for (int i = 0; i < polygon.Length; i++)
-            { 
-                    if (!polygon[i].IsHorizontalLine() && !polygon[i].IsVerticalLine()) 
-                        counter++;  
-            }
-            Console.WriteLine($"there are {counter} lines who aren't vertical and not horizontal");
-            bool parallel = false;
-            for (int i = 0; i < polygon.Length; i++)
-            {
-                for (int k = 0; k < polygon.Length; k++)
+                if (name.Equals(group.GetStudents()[i].GetName()))
                 {
-                    if (polygon[i].IsParallelLines(polygon[k]) && i != k)
-                    {
-                        parallel = true;
-                    }
+                    group.GetStudents()[i].GetParent().SetPhone(phone);
+                    return true;
                 }
             }
-            if(parallel)
-                Console.WriteLine("there are some parallel lines");
+            return false;
+        }
+        public static bool AreThereTwins(Class group)
+        {
+            for (int i = 0; i < group.GetMaxAmountStudent(); i++)
+            {
+                for (int k = 0; k < group.GetMaxAmountStudent(); k++)
+                {
+                    if (group.GetStudents()[i].GetParent().Equals(group.GetStudents()[k].GetParent()))
+                        return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Class c1 = new Class("iud alef4", 4);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Parent name: ");
+                string p_name = Console.ReadLine();
+                Console.Write("Parent phone number: ");
+                string phone = Console.ReadLine();
+                Parent parent = new Parent(p_name, phone);
+                Console.Write("Student name: ");
+                string s_name = Console.ReadLine();
+                Console.Write("Student gender: ");
+                string gender = Console.ReadLine();
+                Console.Write("Student age: ");
+                int age = int.Parse(Console.ReadLine());
+                Student student = new Student(s_name, gender, age, parent);
+                bool add = c1.AddStudent(student);
+                if (add)
+                    Console.WriteLine("Successfuly add!\n-------------------------");
+                else
+                    Console.WriteLine("The list is full\n-------------------------");
+            }
+            Console.WriteLine(c1);
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("search student by name: ");
+                string search_name = Console.ReadLine();
+                Student search_student = c1.SearchStudent(search_name);
+                if (search_student == null)
+                    Console.WriteLine("the student you are looking for isn't exist");
+                else
+                    Console.WriteLine($"the student you are looking for is: {search_student}");
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("delete student by name: ");
+                string delete_name = Console.ReadLine();
+                Student delete_student = c1.SearchStudent(delete_name);
+                if (delete_student == null)
+                    Console.WriteLine("the student you are looking for isn't exist");
+                else
+                    Console.WriteLine($"the student you just deleted is: {delete_student}");
+                Console.WriteLine(c1);
+            }
+            ShowParents(c1);
+            Console.Write("enter the name of the student(to update his parent's phone number): ");
+            string name = Console.ReadLine();
+            Console.Write("enter the updated phone number: ");
+            string updated_phone = Console.ReadLine();
+            bool update_success = UpdateParentPhone(c1, name, updated_phone);
+            if (update_success)
+                Console.WriteLine("The update was successful");
             else
-                Console.WriteLine("there are 0 parallel lines");
-            double p = 0;
-            for (int i = 0; i < polygon.Length; i++)
-                p += polygon[i].LengthLine();
-            Console.WriteLine($"the circumference of the polygon is {p}");
-            Line connector = new Line(polygon[0].GetStartPoint().GetX(), polygon[0].GetStartPoint().GetY(), polygon[4].GetStartPoint().GetX(), polygon[4].GetStartPoint().GetY());
-            Console.WriteLine("new line betwin first point of line number 1 and first point of line number 5: " + connector);
+                Console.WriteLine("can't update. the name is not exist");
+            bool are_they_twins = AreThereTwins(c1);
+            if (are_they_twins)
+                Console.WriteLine("There are twins in the class");
+            else
+                Console.WriteLine("You don't have twins in the class");
             Console.ReadKey();
         }
     }
---
+++ everylessons/everylessons/everylessons/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everylessons
+{
+    class Student
+    {
+        string name, gender;
+        int age;
+        Parent parent;
+
+        public Student(string name, string gender, int age, Parent parent)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.parent = parent;
+        }
+        public string GetName() => name;
+        public string GetGender() => gender;
+        public int GetAge() => age;
+        public Parent GetParent() => parent;
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+        public override string ToString()
+        {
+            return $"name: {name} \ngender: {gender} \nage: {age}\n{parent}\n--------------------\n";
+        }
+    }
+}
