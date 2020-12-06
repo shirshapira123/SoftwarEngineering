14f63dc790192eb94705cab7ed6bac346eaea44b
shir <b33860@ganyavne.ort.org.il>
2020-12-06 10:05:14 +0200

    list - first lesson

---
+++ everylessons/everylessons/everylessons/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everylessons
+{
+    class Node<T>
+    {
+        T info;
+        Node<T> next;
+
+        public Node(T info, Node<T> next)
+        {
+            this.info = info;
+            this.next = next;
+        }
+        public Node(T info)
+        {
+            this.info = info;
+            this.next = null;
+        }
+        public void SetInfo(T info)
+        {
+            this.info = info;
+        }
+        public void SetNext(Node<T> next)
+        {
+            this.next = next;
+        }
+        public T GetInfo() => this.info;
+        public Node<T> GetNext() => this.next;
+    }
+}
